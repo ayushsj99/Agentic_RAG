e542bbae9d9fdc93a0ddedf551d9c4a5f31d0021
Ayush Jadhav <ayushsjadhav99@gmail.com>
2026-02-10 03:10:42 +0530

    ingestion pipeline tested and done

--- database/milvus_config.py
+++ database/milvus_config.py
@@ -1,10 +1,13 @@
 from langchain_milvus import Milvus
 from models.gemini_emb_model import embeddings
 
-URI="./milvus_example.db"
+URI = "http://localhost:19530"
 
 vector_store = Milvus(
     embedding_function=embeddings,
-    connection_args={"uri": URI},
+    connection_args={"uri": URI, "token": "root:Milvus", "db_name": "milvus_demo"},
     index_params={"index_type": "FLAT", "metric_type": "L2"},
+    consistency_level="Strong",
+    collection_name="agentic_rag_docs",
+    drop_old=False,  
 )
